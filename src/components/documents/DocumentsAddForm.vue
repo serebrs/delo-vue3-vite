30ccbf4527ba2c5072fcb81f17fc5ad7d8ec9073
serebrs <serebr.s@gmail.com>
2022-03-31 16:19:19 +0600

    Вынес часть стелей

--- src/components/documents/DocumentsAddForm.vue
+++ src/components/documents/DocumentsAddForm.vue
@@ -5,12 +5,11 @@ export default {
 </script>
 
 <template>
-  <div class="flex flex-col justify-start items-start space-y-6 w-full bg-gray-100 rounded-md p-5">
-    <div class="flex space-x-8">
-      <label class="w-full">
-        <span class="text-gray-600 text-xs font-semibold mb-1 block">Тип</span>
-        <select class="text-sm leading-5 px-3 py-2 w-full">
-          <option selected value="all">Все</option>
+  <div class="flex flex-col space-y-6 w-full bg-gray-100 rounded-md p-5">
+    <div class="flex flex-row space-x-3 sm:space-x-8">
+      <label class="w-96 sm:w-full">
+        <span class="span-label">Тип</span>
+        <select class="input-style">
           <option>Иванов И.И.</option>
           <option>Сидоров С.С.</option>
           <option>Васильев В.В.</option>
@@ -18,25 +17,25 @@ export default {
         </select>
       </label>
 
-      <label class="w-full">
-        <span class="text-gray-600 text-xs font-semibold mb-1 block">Номер</span>
-        <input type="text" class="text-sm leading-5 px-3 py-2 w-full" />
+      <label class="w-56 sm:w-full">
+        <span class="span-label">Номер</span>
+        <input type="text" class="input-style" />
       </label>
 
       <label class="w-full">
-        <span class="text-gray-600 text-xs font-semibold mb-1 block">Дата</span>
-        <input type="date" class="text-sm leading-5 px-3 py-2 w-full" />
+        <span class="span-label">Дата</span>
+        <input type="date" class="input-style" />
       </label>
     </div>
 
     <label class="w-full">
-      <span class="text-gray-600 text-xs font-semibold mb-1 block">Название</span>
-      <input type="text" class="text-sm leading-5 px-3 py-2 w-full" />
+      <span class="span-label">Название</span>
+      <input type="text" class="input-style" />
     </label>
 
-    <label class="w-full">
-      <span class="text-gray-600 text-xs font-semibold mb-1 block">Ответственные (можно выбрать несколько)</span>
-      <select multiple class="text-sm leading-5 px-3 py-2 h-48 w-full">
+    <label class="w-48">
+      <span class="span-label">Ответственные (можно выбрать несколько)</span>
+      <select multiple class="input-style h-48">
         <option>Иванов И.И.</option>
         <option>Сидоров С.С.</option>
         <option>Васильев В.В.</option>
@@ -45,3 +44,13 @@ export default {
     </label>
   </div>
 </template>
+
+<style scoped>
+  .span-label {
+    @apply text-gray-600 text-xs font-semibold mb-1 block;
+  }
+
+  .input-style {
+    @apply text-sm leading-5 px-3 py-2 w-full;
+  }
+</style>
--- src/components/utils/ModalBoxButton.vue
+++ src/components/utils/ModalBoxButton.vue
@@ -62,7 +62,7 @@ export default {
         @click.self="isModalBoxActive = false"
         class="absolute overflow-y-auto z-40 inset-y-0 inset-x-0 flex flex-col justify-start items-center space-x-0 space-y-0 h-screen bg-slate-700 bg-opacity-50 overflow-hidden backdrop-blur-sm"
       >
-        <div class="relative w-[32rem] sm:w-full sm:max-w-3xl p-10 rounded-md my-10 shadow-md bg-white border border-gray-100">
+        <div class="relative w-[32rem] sm:w-full sm:max-w-3xl p-5 sm:p-10 rounded-md my-10 shadow-md bg-white border border-gray-100">
           <button
             @click="isModalBoxActive = false"
             class="absolute right-0 top-0 p-1 m-1 text-gray-600 bg-gray-100 hover:bg-gray-200 rounded-md"
