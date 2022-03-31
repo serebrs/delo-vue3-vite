a03f7ea62380fd8a7135caece7f7d47e69dc4825
serebrs <serebr.s@gmail.com>
2022-03-31 16:22:57 +0600

    .

--- src/components/documents/DocumentsAddForm.vue
+++ src/components/documents/DocumentsAddForm.vue
@@ -8,8 +8,8 @@ export default {
   <div class="flex flex-col space-y-6 w-full bg-gray-100 rounded-md p-5">
     <div class="flex flex-row space-x-3 sm:space-x-8">
       <label class="w-96 sm:w-full">
-        <span class="span-label">Тип</span>
-        <select class="input-style">
+        <span class="modal-span-label">Тип</span>
+        <select class="modal-input-style">
           <option>Иванов И.И.</option>
           <option>Сидоров С.С.</option>
           <option>Васильев В.В.</option>
@@ -18,24 +18,24 @@ export default {
       </label>
 
       <label class="w-56 sm:w-full">
-        <span class="span-label">Номер</span>
-        <input type="text" class="input-style" />
+        <span class="modal-span-label">Номер</span>
+        <input type="text" class="modal-input-style" />
       </label>
 
       <label class="w-full">
-        <span class="span-label">Дата</span>
-        <input type="date" class="input-style" />
+        <span class="modal-span-label">Дата</span>
+        <input type="date" class="modal-input-style" />
       </label>
     </div>
 
     <label class="w-full">
-      <span class="span-label">Название</span>
-      <input type="text" class="input-style" />
+      <span class="modal-span-label">Название</span>
+      <input type="text" class="modal-input-style" />
     </label>
 
     <label class="w-48">
-      <span class="span-label">Ответственные (можно выбрать несколько)</span>
-      <select multiple class="input-style h-48">
+      <span class="modal-span-label">Ответственные (можно выбрать несколько)</span>
+      <select multiple class="modal-input-style h-48">
         <option>Иванов И.И.</option>
         <option>Сидоров С.С.</option>
         <option>Васильев В.В.</option>
@@ -44,13 +44,3 @@ export default {
     </label>
   </div>
 </template>
-
-<style scoped>
-  .span-label {
-    @apply text-gray-600 text-xs font-semibold mb-1 block;
-  }
-
-  .input-style {
-    @apply text-sm leading-5 px-3 py-2 w-full;
-  }
-</style>
